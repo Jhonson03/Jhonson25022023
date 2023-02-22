c6b72f86f2a97b8bb58244889cfbdbeffeb15965
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-22 14:34:56 -0600

    Parte vista

--- AsincronaS5/Program.cs
+++ AsincronaS5/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using AsincronaS5.Entidades;
+using AsincronaS5.Negocio;
+
+Datos dat = new Datos();
+ClsDatos clsDatos = new ClsDatos();
+
+Console.Write(@"Bienvenido al mostrador de numeros pares
+
+Por favor ingrese el valor inicial: ");
+
+dat.inicio = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nIngrese el valor final: ");
+dat.fin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"\nLos números pares son: {clsDatos.Contador(dat)}");
