1e8f3508edc05d72b02a5a0764287a9d3771e26f
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-22 14:34:38 -0600

    Parte Logica

--- AsincronaS5/Negocio/ClsDatos.cs
+++ AsincronaS5/Negocio/ClsDatos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsincronaS5.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace AsincronaS5.Negocio
 {
     public class ClsDatos
     {
+        public int Contador(Datos dat)
+        {
+            for (int i = dat.inicio; i <= dat.fin; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0} ", i);
+                    dat.count++;
+                }
+            }
+            return dat.count;
+        }
     }
 }
